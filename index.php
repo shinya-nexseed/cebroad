80ad87665794955e687b51c2ba916125f12354ce
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-06-05 21:15:31 +0800

    微調整、写真変更（素材サイトからの写真）、ログインしていたらイベント詳細へ飛ばす処理

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
+<?php 
+//ログイン判定
+	//セッションにidが存在し、かつオンのtimeと3600秒足した値が現在時刻より小さい時に
+	//現在時刻より小さい時にログインしていると判定する
+	if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
+	//$_SESSIONに保存している時間更新
+	//これがないとログインから１時間たったら再度ログインしないとindex.phpに入れなくなる。
+	$_SESSION['time'] = time();
+	//event/show.phpへ遷移
+	header('Location: event/show.php'); 
+    exit(); 
+    }else{
+    	echo'ログイン判定(ログインしていない)';
+    	echo'<br>';
+    }
+ ?>
+
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
@@ -44,77 +55,82 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				});
 		</script>
 </head>
+
 <body>
   index.php通過
     <div id="top" class="navbar navbar-inverse" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-           <a class="nav-icon" href="#"> <img src="webroot/assets/images/nav-icon.png " /> </a>
-			<span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Cebroad</a>
-        </div>
-        <div class="collapse navbar-collapse navbar-right">
-          <ul class="nav navbar-nav">
-            <!-- <li class="active"> -->
-          <!--   <li><a href="#">Help</a></li> -->
-            <li><a href="join/signup.php">Sign up</a></li>
-            <li><a href="signin.php">Login</a></li>
-          <!--   <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#contact">Contact</a></li> -->
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-      <!----start-slider---->
-         <!----slider-requried-files--->
-        <!----//slider-requried-files--->
-        <!---start-image-slider---->
-		<div class="slideshow">
-		    <div>
-		     <h1>Beyond Schools</h1>
-		     <span class="border"> </span>
-		     <p>Don't be stuck at your school. Come out!</p>
+    <div class="overlay">
+		<div class="container">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		       <a class="nav-icon" href="#"> <img src="webroot/assets/images/nav-icon.png " /> </a>
+				<span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="index.php">Cebroad</a>
 		    </div>
-		 <!--    <div>
-		     <h1>Get together beyond school</h1>
-		     <span class="border"> </span>
-		     <p>join and make your Cebu life more fun</p>
-		   </div> -->
+		    <div class="collapse navbar-collapse navbar-right">
+		      <ul class="nav navbar-nav">
+		        <!-- <li class="active"> -->
+		        <li><a href="aboutus.php">About us</a></li>
+		        <li><a href="join/signup.php">Sign up</a></li>
+		        <li><a href="signin.php">Login</a></li>
+		        <!-- <li><a href="#portfolio">Portfolio</a></li>
+		        <li><a href="#contact">Contact</a></li> -->
+		      </ul>
+		    </div><!--/.nav-collapse -->
+		    <!--start-slider-->
+		     <!--slider-requried-files
+		    slider-requried-files
+		    start-image-slider-->
+			<div class="slideshow">
+			    <div>
+			     <h1>Cebu local SNS</h1>
+			     <span class="border"> </span>
+			     <p>Join the getherings that interest you and meet new friends beyond school</p>
+			    </div>
+			    <!-- <div>
+			     <h1>Get together beyond school</h1>
+			     <span class="border"> </span>
+			     <p>join and make your Cebu life more fun</p>
+			    </div> -->
+			</div>
+			<div class="hero__content-footer">
+			<div class="go_btn">
+			    <a class="button" href="join/signup.php">Sign up</a>
+			</div>
+			</div>
+			<!-- -start-slider-script- -->
+			<!--<script>
+				// $(function() {
+				// 	$(".slideshow > div:gt(0)").hide();
+				// 	setInterval(function() { 
+				// 	  $('.slideshow > div:first')
+				// 	    .fadeOut(1000)
+				// 	    .next()
+				// 	    .fadeIn(1000)
+				// 	    .end()
+				// 	    .appendTo('.slideshow');
+				// 	},  3000);
+					
+				// });
+			</script>-->
+			<!---//End-slider-script-->
+		    <!--//End-slider-->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/bootstrap.min.js"></script>
 		</div>
-		<div class="go_btn">
-		    <a class="button" href="join/signup.php">Sign up</a>
-		</div>
-		<!-----start-slider-script---->
-<!-- 		<script>
-			$(function() {
-				$(".slideshow > div:gt(0)").hide();
-				setInterval(function() { 
-				  $('.slideshow > div:first')
-				    .fadeOut(1000)
-				    .next()
-				    .fadeIn(1000)
-				    .end()
-				    .appendTo('.slideshow');
-				},  3000);
-				
-			});
-		</script> -->
-		<!-----//End-slider-script---->
 	</div>
-        <!----//End-slider---->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+	</div>
     <!----//End-container---->
-  		<!----//-container---->
+  	<!----//-container---->
 <div  class="about me" id="about">  
 		<div class="container">
 			<div class="row content-top">
 				<div class="col-md-12  text-center">
  					<h2>SNS for foreign students in Cebu</h2>
- 					 <p>If you are looking for new friends or trying to extend your network here in Cebu, you can search new friends who are also studying here in CEBU. You can join the events that someone creates on Ceburoad or you can also host events.</p>
+ 					 <p>If you're feeling like meeting up with new people beyond your network, search getherings that may interests you on Cebroad. Then you get to know new friends who are also studying here.</p>
  				</div>	
 			</div>
 		</div>
@@ -126,17 +142,17 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="col-md-6 text-center">
 					<div class="grid ">
 						<!-- <img src="webroot/assets/images/icon1.png" alt=""/> -->
-						<i class="fa fa-building fa-large" aria-hidden="true"></i>
-						<h3>Search events</h3>
-						<p>You can search events and contact the organizer. Join and meet new people. Don't be bored come out.</p>
+						<i class="fa fa-search fa-large" aria-hidden="true"></i>
+						<h3>Search gatherings</h3>
+						<p>You can search gatherings and contact the organizer. Join and meet new people. Don't be bored at your dorm.</p>
 					</div>
 				</div>
 					<div class="col-md-6 text-center">
 						<div class="grid ">
 							<!-- <img src="webroot/assets/images/icon2.png" alt=""/> -->
 							<i class="fa fa-users fa-large" aria-hidden="true"></i>
-							<h3>Create events</h3>
-							<p>You can also create events. Share your interests and thoughts, and finde new friends who have same interest as you.</p>
+							<h3>Create gatherings</h3>
+							<p>You can also create gatherings. It does't need to be formal events but anything including casual dinner, travel, study group etc. Have fun and meet new friends.</p>
 						</div>
 					</div>
 					<!-- <div class="col-md-4 text-center">
@@ -155,7 +171,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<div class="container">
 			<div class="row content-top">
 				<div class="col-md-12  text-center">
- 					<h2>Upcomming Events</h2>
+ 					<h2>Upcomming gatherings</h2>
 			</div>
 		</div>
 	</div>
@@ -181,7 +197,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						</li>
 						<li class="col-md-6">
 							<figure>
-								<img class="responsive-img img1" src="webroot/assets/images/cala2.jpg	" alt="img01">
+								<img class="responsive-img img1" src="webroot/assets/images/cala.jpg	" alt="img01">
 								<figcaption>
 									<h4>Lorem ipsum dolor sit </h4>
 									<span>amet conseteter sadipscing elitr</span>
@@ -191,7 +207,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						</li>
 						<li class="col-md-6">
 							<figure>
-								<img class="responsive-img img1" src="webroot/assets/images/cala.jpg" alt="img02">
+								<img class="responsive-img img1" src="webroot/assets/images/cala2.jpg" alt="img02">
 								<figcaption>
 									<h4>Lorem ipsum dolor sit </h4>
 									<span>amet conseteter sadipscing elitr</span>
@@ -267,7 +283,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				</div> -->
 				<div class="row copy-right">
 					<div class="col-md-12 text-center">
-						<p>Copyright &#169; 2014 All Rights Reserved |<!-- Template by &nbsp;<a href="http://w3layouts.com">W3Layouts --></a></p>
+						<p>Copyright &#169; 2014 All Rights Reserved | Team 0404<!-- Template by &nbsp;<a href="http://w3layouts.com">W3Layouts --></a></p>
 					</div>
 				</div>
 			</div>
