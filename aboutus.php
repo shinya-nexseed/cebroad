a8bd844046eb22f539c5ac3745441ee9b5fcbc23
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-06-05 21:16:35 +0800

    about us, terms, privacy policyのドラフト

---
+++ aboutus.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+    <title>Cebroad</title>
+		<link href="webroot/assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="webroot/assets/css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+		<link href="webroot/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+		<link href="webroot/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="webroot/assets/js/bootstrap.min.js"></script>
+		<script src="webroot/assets/js/jquery.min.js"></script>
+	<!-- Custom files -->
+		<link href="webroot/assets/css/sitetop_style.css" rel='stylesheet' type='text/css' />
+   	<!-- Custom Theme files -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		</script>
+	<!----//webfonts---->
+		<link href='http://fonts.googleapis.com/css?family=Rokkitt:400,700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
+	<!----//webfonts---->
+		<script type="text/javascript">
+				$(function() {
+				  $('a[href*=#]:not([href=#])').click(function() {
+				    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			
+				      var target = $(this.hash);
+				      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				      if (target.length) {
+				        $('html,body').animate({
+				          scrollTop: target.offset().top
+				        }, 1000);
+				        return false;
+				      }
+				    }
+				  });
+				});
+		</script>
+</head>
+
+<body>
+  index.php通過
+    <div id="top" class="navbar navbar-inverse" role="navigation">
+    <div class="overlay">
+		<div class="container">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		       <a class="nav-icon" href="#"> <img src="webroot/assets/images/nav-icon.png " /> </a>
+				<span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="index.php">Cebroad</a>
+		    </div>
+		    <div class="collapse navbar-collapse navbar-right">
+		      <ul class="nav navbar-nav">
+		        <!-- <li class="active"> -->
+		        <li><a href="aboutus.php">About us</a></li>
+		        <li><a href="join/signup.php">Sign up</a></li>
+		        <li><a href="signin.php">Login</a></li>
+		        <!-- <li><a href="#portfolio">Portfolio</a></li>
+		        <li><a href="#contact">Contact</a></li> -->
+		      </ul>
+		    </div><!--/.nav-collapse -->
+			<div class="slideshow">
+			    <div>
+			     <h1>Team 0404</h1>
+			     <span class="border"> </span>
+			     <p>Hi all, thank you for visiting this page. Team0404 is a five member group and we created this SNS as an assignment of a program we were in. We are also studying in CEBU like you. So we hope to see you somewhere soon.</p>
+			    </div>
+			</div>
+			<script src="js/jquery.min.js"></script>
+			<script src="js/bootstrap.min.js"></script>
+		</div>
+	</div>
+	</div>
+    <!----//End-container---->
+  	<!----//-container---->
+
+		<div class="footer">
+			<div class="container">
+				<div class="row copy-right">
+					<div class="col-md-12 text-center">
+						<p>Copyright &#169; 2014 All Rights Reserved | Team 0404<!-- Template by &nbsp;<a href="http://w3layouts.com">W3Layouts --></a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
