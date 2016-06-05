d8128e9d3065b7a5ed8eae4fab6cff53322ddf69
竹田美雪 <takeda.myk@gmail.com>
2016-06-05 14:46:31 +0800

    signupページのHTML・CSS変更

---
+++ join/signup.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Signup</title>
+
+    <!-- Bootstrap -->
+    <link href="../webroot/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="../webroot/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../webroot/assets/css/form.css" rel="stylesheet">
+    <link href="../webroot/assets/css/timeline.css" rel="stylesheet">
+    <link href="../webroot/assets/css/signup.css" rel="stylesheet">
+    <link href="../webroot/assets/css/main.css" rel="stylesheet">
+    <link href="../webroot/assets/js/signup.js" rel="stylesheet">
+  
+    </head>
+      <body>
+      <div class="container">
+          <div class="row">
+          <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-login">
+              <div class="panel-heading">
+                <div class="row">
+                  <a href="#" id="register-form-link">SIGNUP</a>
+                </div>
+                <hr>
+              </div>
+              <div class="panel-body">
+                <div class="row">
+                  <div class="col-lg-12">
+                  <form id="register-form" action="" method="post" role="form" style="display: block;">
+                      <div class="form-group">
+                        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Nickname" value="">
+                      </div>
+                      <div class="form-group">
+                        <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+                      </div>
+                      <div class="form-group">
+                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                      </div>
+                      <div class="form-group">
+                        <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+                      </div>
+                      <div class="form-group">
+                        <div class="row">
+                          <div class="col-sm-6 col-sm-offset-3">
+                            <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="SIGNUP">
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
