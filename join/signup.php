b15c76b35677f0708afd504dcb6b5f8f80f6047a
竹田美雪 <takeda.myk@gmail.com>
2016-06-06 11:12:39 +0800

    バリデーション追加(パスワードと確認用パスワードが一致するか)

--- join/signup.php
+++ join/signup.php
@@ -23,8 +23,10 @@
     } else if(strlen($_POST['password']) < 4){
       $error['password'] = 'length';
     }
+    if(($_POST['password']) !== ($_POST['confirm_password'])){
+      $error['confirm_password'] = 'incorrect';
+    }
   }
-
   //htmlspecialcharsのショートカット
   function h($value){
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
@@ -40,66 +42,69 @@
   
       <div class="container">
           <div class="row">
-          <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-login">
-              <div class="panel-heading">
-                <div class="row">
-                  <a href="#" id="register-form-link">SIGNUP</a>
+            <div class="col-md-6 col-md-offset-3">
+              <div class="panel panel-login">
+                <div class="panel-heading">
+                  <div class="row">
+                    <a href="#" id="register-form-link">SIGNUP</a>
+                  </div>
+                  <hr>
                 </div>
-                <hr>
-              </div>
               <div class="panel-body">
                 <div class="row">
                   <div class="col-lg-12">
                   <form id="register-form" action="" method="post" role="form" style="display: block;">
                     <!--ニックネーム-->
-                    <div class="form-group">
-                      <?php if(isset($_POST['nick_name'])): ?>
-                        <input type="text" name="nick_name" id="nick_name" tabindex="1" class="form-control" placeholder="Nickname" value="<?php echo h($_POST['nick_name']); ?>">
-                      <?php else: ?>
-                        <input type="text" name="nick_name" id="nick_name" tabindex="1" class="form-control" placeholder="Nickname" value="">
-                      <?php endif; ?>
-                      <!-- ニックネームが空欄だったら -->
-                      <?php if(isset($error['nick_name']) && $error['nick_name'] == 'blank'): ?>
-                        <p class="error">＊Please type your Nicknake.</p>
-                      <?php endif; ?>
+                      <div class="form-group">
+                        <?php if(isset($_POST['nick_name'])): ?>
+                          <input type="text" name="nick_name" id="nick_name" tabindex="1" class="form-control" placeholder="Nickname" value="<?php echo h($_POST['nick_name']); ?>">
+                        <?php else: ?>
+                          <input type="text" name="nick_name" id="nick_name" tabindex="1" class="form-control" placeholder="Nickname" value="">
+                        <?php endif; ?>
+                        <!-- ニックネームが空欄だったら -->
+                        <?php if(isset($error['nick_name']) && $error['nick_name'] == 'blank'): ?>
+                          <p class="error">* Please type your Nicknake.</p>
+                        <?php endif; ?>
                       </div>
                     <!--メールアドレス-->
                       <div class="form-group">
-                      <?php if (isset($_POST['email'])): ?>
-                        <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="<?php echo h($_POST['email']); ?>">
+                        <?php if (isset($_POST['email'])): ?>
+                          <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="<?php echo h($_POST['email']); ?>">
                         <?php else: ?>
-                        <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+                          <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
                         <?php endif; ?>
-                      <!-- メールアドレスが空欄だったら -->
-                      <?php if(isset($error['email']) && $error['email'] == 'blank'): ?>
-                        <p class="error">＊Please type your Email Address.</p>
-                      <?php endif; ?>
+                        <!-- メールアドレスが空欄だったら -->
+                        <?php if(isset($error['email']) && $error['email'] == 'blank'): ?>
+                          <p class="error">* Please type your Email Address.</p>
+                        <?php endif; ?>
                       </div>
                     <!--パスワード-->
                       <div class="form-group">
-                      <?php if(isset($_POST['password'])): ?>
-                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" value="<?php echo h($_POST['password']); ?>">
-                      <?php else: ?>
+                        <?php if(isset($_POST['password'])): ?>
+                          <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" value="<?php echo h($_POST['password']); ?>">
+                        <?php else: ?>
                           <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" value="">
-                      <?php endif; ?>
-                      <?php if(isset($error['password']) && $error['password'] == 'blank'): ?>
-                        <p class="error">＊Please type Password.</p>
-                      <?php endif; ?>
-                      <?php if(isset($error['password']) && $error['password'] == 'length'): ?>
-                         <p class="error">＊Please type Password more than 4 letters.</p>
-                      <?php endif; ?>
+                        <?php endif; ?>
+                        <?php if(isset($error['password']) && $error['password'] == 'blank'): ?>
+                          <p class="error">* Please type Password.</p>
+                        <?php endif; ?>
+                        <?php if(isset($error['password']) && $error['password'] == 'length'): ?>
+                         <p class="error">* Please type Password more than 4 letters.</p>
+                        <?php endif; ?>
                       </div>
-
+                    <!--2つのパスワードが一致するか確認-->
                       <div class="form-group">
-                      <?php if(isset($_POST['password'])): ?>
-                        <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password" value="<?php echo h($_POST['password']); ?>">
-                      <?php else: ?>
-                          <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password" value="">
-                      <?php endif; ?>
-                      <?php if(isset($error['password']) && $error['password'] == 'blank'): ?>
-                        <p class="error">＊Please type Password.</p>
-                      <?php endif; ?>
+                        <?php if(isset($_POST['confirm_password'])): ?>
+                          <input type="password" name="confirm_password" id="confirm_password" tabindex="2" class="form-control" placeholder="Confirm Password" value="<?php echo h($_POST['confirm_password']); ?>">
+                        <?php else: ?>
+                            <input type="password" name="confirm_password" id="confirm_password" tabindex="2" class="form-control" placeholder="Confirm Password" value="">
+                        <?php endif; ?>
+                        <?php if(isset($error['confirm_password']) && $error['confirm_password'] == 'blank'): ?>
+                          <p class="error">* Please type Password.</p>
+                        <?php endif; ?>
+                        <?php if (isset($error['confirm_password']) && $error['confirm_password'] == 'incorrect'): ?>
+                          <p class="error">* The 2 Password do not match.</p>
+                        <?php endif; ?>
                       </div>
                      
                       <div class="form-group">
