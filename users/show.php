1353c37723dac04d9b2e0008ef960a0776436a56
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-06-08 11:50:51 +0800

    Bootsnipのテンプレ取得、CSSあてた。

---
+++ users/show.php
@@ -0,0 +1,140 @@
+<?php
+//データベース接続
+  require('dbconnect.php');
+
+//セッションを使うページに必ず入れる
+  // session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="../webroot/assets/css/users_show.css" rel="stylesheet">
+	<link href="../webroot/assets/css/bootstrap.min.css" rel="stylesheet">
+	<script type="text/javascript">
+		    $(document).ready(function() {
+		    var panels = $('.user-infos');
+		    var panelsButton = $('.dropdown-user');
+		    panels.hide();
+
+		    //Click dropdown
+		    panelsButton.click(function() {
+		        //get data-for attribute
+		        var dataFor = $(this).attr('data-for');
+		        var idFor = $(dataFor);
+
+		        //current button
+		        var currentButton = $(this);
+		        idFor.slideToggle(400, function() {
+		            //Completed slidetoggle
+		            if(idFor.is(':visible'))
+		            {
+		                currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
+		            }
+		            else
+		            {
+		                currentButton.html('<i class="glyphicon glyphicon-chevron-down text-muted"></i>');
+		            }
+		        })
+		    });
+
+
+		    $('[data-toggle="tooltip"]').tooltip();
+
+		    $('button').click(function(e) {
+		        e.preventDefault();
+		        alert("This is a demo.\n :-)");
+		    });
+		});
+	</script>
+</head>
+<body>
+<div class="container">
+     <div class="row">
+      	<div class="col-md-5  toppad  pull-right col-md-offset-3 ">
+         	<A href="edit.html" >Edit Profile</A>
+
+        	<A href="edit.html" >Logout</A>
+        	<br>
+			<p class=" text-info">May 05,2014,03:00 pm </p>
+      	</div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+   
+   
+         	<div class="panel panel-info">
+            	<div class="panel-heading">
+              		<h3 class="panel-title">Sheena Kristin A.Eschor</h3>
+            	</div>
+	            <div class="panel-body">
+	              	<div class="row">
+	                	<div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="http://babyinfoforyou.com/wp-content/uploads/2014/10/avatar-300x300.png" class="img-circle img-responsive"> </div>
+	                
+		                <!--<div class="col-xs-10 col-sm-10 hidden-md hidden-lg"> <br>
+		                  <dl>
+		                    <dt>DEPARTMENT:</dt>
+		                    <dd>Administrator</dd>
+		                    <dt>HIRE DATE</dt>
+		                    <dd>11/12/2013</dd>
+		                    <dt>DATE OF BIRTH</dt>
+		                       <dd>11/12/2013</dd>
+		                    <dt>GENDER</dt>
+		                    <dd>Male</dd>
+		                  </dl>
+		                </div>-->
+		                <div class=" col-md-9 col-lg-9 "> 
+		                  	<table class="table table-user-information">
+			                    <tbody>
+			                      <tr>
+			                        <td>Department:</td>
+			                        <td>Programming</td>
+			                      </tr>
+			                      <tr>
+			                        <td>Hire date:</td>
+			                        <td>06/23/2013</td>
+			                      </tr>
+			                      <tr>
+			                        <td>Date of Birth</td>
+			                        <td>01/24/1988</td>
+			                      </tr>
+			                   
+			                         <tr>
+			                             <tr>
+			                        <td>Gender</td>
+			                        <td>Male</td>
+			                      </tr>
+			                        <tr>
+			                        <td>Home Address</td>
+			                        <td>Metro Manila,Philippines</td>
+			                      </tr>
+			                      <tr>
+			                        <td>Email</td>
+			                        <td><a href="mailto:info@support.com">info@support.com</a></td>
+			                      </tr>
+			                        <td>Phone Number</td>
+			                        <td>123-4567-890(Landline)<br><br>555-4567-890(Mobile)
+			                        </td>
+			                           
+			                      </tr>
+			                     
+			                    </tbody>
+		                  	</table>
+			                  
+			                <a href="#" class="btn btn-primary">My Sales Performance</a>
+			                <a href="#" class="btn btn-primary">Team Sales Performance</a>
+		                </div>
+	            	</div>
+           		</div>
+	            <div class="panel-footer">
+                    <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
+                    <span class="pull-right">
+                        <a href="edit.html" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
+                        <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
+                    </span>
+	            </div>            
+        	</div>
+    	</div>
+	</div>
+</div> 
+</body>
+</html>
