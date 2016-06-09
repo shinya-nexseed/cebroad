c8cf89523a6c55279d71d2936009b59bd7a70866
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-06-09 09:41:37 +0800

    ・表のカラー変更html

--- users/show.php
+++ users/show.php
@@ -94,11 +94,11 @@
 <body>
 <div class="container">
      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
    
    
-         	<div class="panel panel-info">
-            	<div class="panel-heading">
+         	<div class="panel panel-info" id="panel-color">
+            	<div class="panel-heading" id="panel-color">
               		<h3 class="panel-title"><?php echo h($user['nick_name']); ?></h3>
             	</div>
 	            <div class="panel-body">
