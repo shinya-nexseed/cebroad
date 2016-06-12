be6b43e5ff22685b737dfa8e743f02c5c6b33d4c
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-06-12 17:43:59 +0800

    ・Terms and Condition, Privacy policy清書

--- index.php
+++ index.php
@@ -447,39 +447,15 @@
         <div class="footer-area">
             <div class="container">
                 <div class="row footer">
-                <!--     <div class="col-md-4">
-                        <div class="single-footer">
-                            <img src="img/footer-logo.png" alt="" class="wow pulse" data-wow-delay="1s">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati architecto quaerat facere blanditiis tempora sequi nulla accusamus, possimus cum necessitatibus suscipit quia autem mollitia, similique quisquam molestias. Vel unde, blanditiis.</p>
-                        </div>
-                    </div> -->
-                    <!-- <div class="col-md-6">
-                        <div class="single-footer">
-                            <h4>Twitter update</h4>
-                            <div class="twitter-updates">
-                                <div class="single-tweets">
-                                    <h5>ABOUT 9 HOURS</h5>
-                                    <p><strong>AGOMeet Aldous</strong> - a Brave New World for #rails with more cohesion, less coupling and greater dev speed <a href="http://t.co/rsekglotzs">http://t.co/rsekglotzs</a></p>
-                                </div>
-                                <div class="single-tweets">
-                                    <h5>ABOUT 9 HOURS</h5>
-                                    <p><strong>AGOMeet Aldous</strong> - a Brave New World for #rails with more cohesion, less coupling and greater dev speed <a href="http://t.co/rsekglotzs">http://t.co/rsekglotzs</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
                     <div class="col-md-6">
                         <div class="single-footer">
                             <h4>Useful lnks</h4>
                             <div class="footer-links">
                                 <ul class="list-unstyled">
-                                    <!-- <li><a href="">About Us</a></li>
-                                    <li><a href="" class="active">Services</a></li>
-                                    <li><a href="">Work</a></li>
-                                    <li><a href="">Our Blog</a></li> -->
+                                    <li><a href="index">Home</a></li>
                                     <li><a href="aboutus.php">About us</a></li>
-                                    <li><a href="">Terms and Conditions</a></li>
-                                    <li><a href="">Privacy policy</a></li>
+                                    <li><a href="terms.html">Terms and Conditions</a></li>
+                                    <li><a href="privacypolicy.html">Privacy policy</a></li>
                                     <li><a href="">Contact us</a></li>
                                 </ul>
                             </div>
@@ -487,15 +463,10 @@
                     </div>
                 </div>
                 <div class="row footer-copy">
-                    <p><span>(C) All rights reserved</span> | <span><a href="aboutus.php">Team0404</a></span> <!-- | <span> Web Designed by <a href="http://ohidul.me">Ohidul Islam</a></span> </p> -->
+                    <p><span>(C) All rights reserved</span> | <span><a href="aboutus.php">Cebroad</a></span> <!-- | <span> Web Designed by <a href="http://ohidul.me">Ohidul Islam</a></span> </p> -->
                 </div>
             </div>
-        </div>
-		
-		
-		
-		
-		
+        </div>  	
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="webroot/assets/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
         <script src="webroot/assets/js/bootstrap.min.js"></script>
