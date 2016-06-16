8f8b9b8fae66cce2f077add1f120ea18ae00171f
今陸成 <rikutech@gmail.com>
2016-06-17 03:35:05 +0800

    application.phpに間違ったurlを入力されてもphpのエラーが表示されないようにする処理を記述 このままではマージできないので本来の担当者にコードの記述を依頼する add.phpのバリデーション項目完成 add.jsでconfirmボタンを押せる状態と押せない状態の処理をより厳密に google maps apiで座標を取得している間にloadingのgifアニメーションを表示 confirm.phpで確認用にサーバーに上げる前に画像データを表示

--- events/add.php
+++ events/add.php
@@ -1,5 +1,5 @@
 <?php
-  $error = Array();
+  $errors = Array();
   
   //例外を返す関数
   function e($message, $previous = null) {
@@ -31,89 +31,175 @@
 
 
   if (!empty($_POST)) {
-
+  try{
+    $e = null;
     if (isset($_POST['title'])) {
         $title = trim(mb_convert_kana($_POST['title'], "s", 'UTF-8'));
-        if ($_POST['title'] === '') {
-            $error['title'] = 'blank';
+        if ($title === '') {
+            $e = e('Please input a title.', $e);
+        } else {
+          $_POST['title'] = $title;
         }
     } else {
-        $error['title']   = 'blank';
+        $e = e('Please input a title.', $e);
     }
 
 
     if (isset($_POST['detail'])) {
-        $title = trim(mb_convert_kana($_POST['detail'], "s", 'UTF-8'));
-        if ($_POST['detail'] === '') {
-            $error['detail'] = 'blank';
+        $detail = trim(mb_convert_kana($_POST['detail'], "s", 'UTF-8'));
+        if ($detail === '') {
+            $e = e('Please input a detail.', $e);
+        } else {
+          $_POST['detail'] = $detail;
         }
     } else {
-        $error['detail']   = 'blank';
+        $e = e('Please input a detail.', $e);
     }
 
     if (empty($_POST['starting_time'])) {
-        $error['starting_time'] = 'blank';
+        $e = e('Please specify a starting time.', $e);
+    }
+
+    if (!empty($_POST['closing_time'])) {
+
     }
 
     if (empty($_POST['place_name']) || empty($_POST['lat']) || empty($_POST['lng'])) {
-        $error['place'] = 'blank';
+        $e = e('Please specify a place.', $e);
     }
 
-    if (isset($_POST['capacity'])) {
-      if (ctype_digit(strval($_POST['capacity']))) {
-          $error['capacity'] = 'type';
+    if (!empty($_POST['capacity'])) {
+      if (!ctype_digit(strval($_POST['capacity']))) {
+          $e = e('The value of the capacity is invalid.', $e);
       }
     }
-    for ($i=1; $i<4; $i++) { 
-    if (empty($_FILES['pic'.$i])) {
-    $fileName = $_FILES['pic'.$i]['name'];
-    if (empty($fileName)) {
-      $ext = substr($fileName, -3);
-      if ($ext !== 'jpg' && $ext !== 'png') {
-        $error['pic'.$i] = 'type';
+
+    for ($i=1; $i<4; $i++) {
+
+    $pic = 'pic'.$i;
+    // 未定義である・複数ファイルである・$_FILES Corruption 攻撃を受けた
+    // どれかに該当していれば不正なパラメータとして処理する
+    if (
+        !isset($_FILES[$pic]['error']) ||
+        !is_int($_FILES[$pic]['error'])
+    ) {
+        $e = e('picture'.$i.'のパラメータが不正です', $e);
+    }
+    if ($_FILES[$pic]['error'] === 0) {
+
+
+    // ここで定義するサイズ上限のオーバーチェック
+    // (必要がある場合のみ)
+    if ($_FILES[$pic]['size'] > 10485760) {
+        $e = e('picture'.$i.'のファイルサイズが大きすぎます', $e);
+    }
+
+
+        // $_FILES['upfile']['mime']の値はブラウザ側で偽装可能なので
+        // MIMEタイプに対応する拡張子を自前で取得する
+        if (!$ext = array_search (
+            mime_content_type($_FILES[$pic]['tmp_name']),
+            array(
+                'jpg' => 'image/jpg',
+                'jpeg' => 'image/jpeg',
+                'png' => 'image/png',
+            ),
+            true
+        )) {
+            $e = e('picture'.$i.'のファイル形式が不正です', $e);
+          }
+
+      $_FILES[$pic]['content'] = file_get_contents($_FILES[$pic]['tmp_name']);
+      $_FILES[$pic]['ext'] = $ext;
+    } else if ($_FILES[$pic]['error'] !== 4) {
+      switch($_FILES[$pic]['error']) {
+        case UPLOAD_ERR_INI_SIZE:  // php.ini定義の最大サイズ超過
+        case UPLOAD_ERR_FORM_SIZE: //form定義の最大サイズ超過
+          $e = e('picture'.$i.'のファイルサイズが大きすぎます。');
+        default:
+          $e = e('picture'.$i.'にその他のエラーが発生しました。');
       }
-     }
-   }
+    }
   }
+    if ($e) {
+      throw $e;
+    }
 
     
-    if (empty($error)) {
+        $_SESSION['events'] = $_POST + $_FILES;
 
-      for ($i=1; $i<4; $i++) {
-      $picture = date('YmdHis') . $_FILES['pic'.$i]['name'];
-      @move_uploaded_file($_FILES['pic'.$i]['tmp_name'], 'events/events_pictures/' . $picture);
-      $_SESSION['events']['pic'.$i] = $picture;
-    }
-      // if (move_uploaded_file($_FILES['picture_path']['tmp_name'], '../member_picture/' . $picture) == false) {
-      //   echo $_FILES['picture_path']['error'];
-      // }
-        $_SESSION['events'] = $_POST;
         header('Location: confirm');
       exit();
-    }
+    } catch (Exception $e) {
+    exception_to_array($e);
   }
+}
+
   if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'rewrite') {
     $_POST = $_SESSION['events'];
-    $error['rewrite'] = true;
+    $errors['rewrite'] = true;
   }
+
+  $title = '';
+  $date = '';
+  $starting_time = '';
+  $closing_time = '';
+  $capacity = '';
+  $place = '';
+  $place_name = '';
+  $lat = '';
+  $lng =  '';
+  $detail = '';
+
+  if ($id === 'rewrite') {
+    $_POST = $_SESSION['events'];
+  }
+
+    if (!empty($_POST['title'])) {
+      $title = $_POST['title'];
+    }
+    if (!empty($_POST['date'])) {
+      $date = $_POST['date'];
+    }
+    if (!empty($_POST['starting_time'])) {
+      $starting_time = $_POST['starting_time'];
+    }
+    if (!empty($_POST['closing_time'])) {
+      $closing_time = $_POST['closing_time'];
+    }
+    if (!empty($_POST['capacity'])) {
+      $title = $_POST['capacity'];
+    }
+    if (!empty($_POST['place'])) {
+      $place = $_POST['place'];
+    }
+    if (!empty($_POST['place_name'])) {
+      $place_name = $_POST['place_name'];
+    }
+    if (!empty($_POST['lat'])) {
+      $lat = $_POST['lat'];
+    }
+    if (!empty($_POST['lng'])) {
+      $lng = $_POST['lng'];
+    }
+    if (!empty($_POST['detail'])) {
+      $detail = $_POST['detail'];
+    }
+
 
 $now = date('Y-m-d');
 $year = date('Y-m-d', strtotime("+1year"));
+var_dump($errors);
  ?>
-<link rel="stylesheet" href="../webroot/assets/css/bootstrap.min.css">
-<link rel="stylesheet" href="../webroot/assets/events/css/events.css">
-<link rel="stylesheet" href="../webroot/assets/font-awesome/css/font-awesome.min.css">
-<script src="../webroot/assets/js/jquery-1.12.4.min.js"></script>
+<script src="/cebroad/webroot/assets/js/jquery-1.12.4.min.js"></script>
 <script src="http://maps.googleapis.com/maps/api/js?libraries=places&output?json&region=ph&language=en&key=AIzaSyDf24saS_c-qe8Qy4QPgVbTub1sJi02ov8"></script>
-<!-- <script src="../webroot/assets/events/js/add.js"></script> -->
-
 
 
 <div class="container">
 
 
     <form method="post" enctype="multipart/form-data">
-    <input type="hidden" name="MAX_FILE_SIZE" value="10240" />
+    <input type="hidden" name="MAX_FILE_SIZE" value="10485760">
 
       <div class="row">
 
@@ -122,34 +208,34 @@ $year = date('Y-m-d', strtotime("+1year"));
 
         <div class="col-sm-8 col-md-8">
             <h1>New event</h1>
-            <h3 class="events_label_color">Red items are necessarily required</h3>
+            <h3 class="cebroad-pink">Red items are necessarily required</h3>
         </div>
 
         <div class="col-sm-8 col-md-8">
             <div class="form-group">
-                <label class="events_label_color">Title</label>
-                <input type="text" name="title" class="form-control" required>
+                <label class="cebroad-pink">Title</label>
+                <input type="text" name="title" id="title" class="form-control" required value="<?=h($title)?>">
             </div>
         </div>
 
         <div class="col-sm-4 col-md-4">
-            <label class="events_label_color">Date</label>
+            <label class="cebroad-pink">Date</label>
             <div class="form-group">
-                <input type="date" name="date" class="form-control " min="<?php echo $now; ?>" max="<?php echo $year; ?>"  required>
+                <input type="date" name="date" id="date" class="form-control " min="<?php echo $now; ?>" max="<?php echo $year; ?>" value="<?=h($date)?>" required>
             </div>
         </div>
 
 
         <div class="col-sm-2 col-md-2">
-            <label class="events_label_color">Starting time</label>
+            <label class="cebroad-pink">Starting time</label>
             <div class="form-group">
-                <input type="time" name="starting_time" class="form-control" required>
+                <input type="time" name="starting_time" id="starting_time" class="form-control" value="<?=h($starting_time)?>" required>
             </div>
         </div>
         <div class="col-sm-2 col-md-2">
                 <label id="closing_time_label">Closing time</label>
                 <div class="form-group">
-                    <input type="time" name="closing_time" class="form-control" id="closing_time">
+                    <input type="time" name="closing_time" id="closing_time" class="form-control" id="closing_time" value="<?=h($closing_time)?>">
                 </div>
 
 <!--             <div class="form-group">
@@ -162,21 +248,21 @@ $year = date('Y-m-d', strtotime("+1year"));
         <div class="col-sm-4 col-md-4">
             <label>Capacity</label>
             <div class="form-group">
-                <input type="number" name="capacity" class="form-control " min="1">
+                <input type="number" name="capacity" id="capacity" class="form-control " min="1" value="<?=h($capacity)?>">
             </div>
         </div>
 
         <div class="col-sm-8 col-md-8">
-            <label class="events_label_color">Place</label>
+            <label class="cebroad-pink">Place</label>
             <div class="form-group">
-                <input id="searchTextField" type="text" name="place" class="form-control" required>
+                <input id="searchTextField" type="text" name="place" id="place" class="form-control" value="<?=h($place)?>" required>
             </div>
         </div>
         
         <div class="col-sm-8 col-md-8">
             <div class="form-group">
-                <label class="events_label_color">detail</label>
-                <textarea name="detail" class="form-control" rows="6" required></textarea>
+                <label class="cebroad-pink">detail</label>
+                <textarea name="detail" id="detail" class="form-control" rows="6" value="<?=h($detail)?>" required></textarea>
             </div>
         </div>
 
@@ -185,10 +271,10 @@ $year = date('Y-m-d', strtotime("+1year"));
             <input class="pic" name="pic1" id="pic1" type="file" style="display:none">
             <div class="input-group">
               <input type="text" id="photoCover1" class="form-control" placeholder="Select jpg or png">
-              <span class="input-group-btn"><button type="button" class="btn btn-info browse" id="browse1" onclick="$('#pic1').click();">Browse</button></span>
+              <span class="input-group-btn"><button type="button" class="btn btn-cebroad" onclick="$('#pic1').click();">Browse</button></span>
             </div>
-            <label id="label1" class="events_label_color"></label>
-            <div class="pad_top">
+            <label id="label1" class="cebroad-pink"></label>
+            <div class="events-pad">
               <img src="" id="preview1" style="display:none; width: 300px;">
             </div>
         </div>
@@ -198,10 +284,10 @@ $year = date('Y-m-d', strtotime("+1year"));
             <input class="pic" name="pic2" id="pic2" type="file" style="display:none">
             <div class="input-group">
               <input type="text" id="photoCover2" class="form-control" placeholder="Select jpg or png">
-              <span class="input-group-btn"><button type="button" class="btn btn-info browse" id="browse2" onclick="$('#pic2').click();">Browse</button></span>
+              <span class="input-group-btn"><button type="button" class="btn btn-cebroad" onclick="$('#pic2').click();">Browse</button></span>
             </div>
-            <label id="label2" class="events_label_color"></label>
-            <div class="pad_top">
+            <label id="label2" class="cebroad-pink"></label>
+            <div class="events-pad">
               <img src="" id="preview2" style="display:none; width: 300px;">
             </div>
         </div>
@@ -211,83 +297,32 @@ $year = date('Y-m-d', strtotime("+1year"));
             <input class="pic" name="pic3" id="pic3" type="file" style="display:none">
             <div class="input-group">
               <input type="text" id="photoCover3" class="form-control" placeholder="Select jpg or png">
-              <span class="input-group-btn"><button type="button" class="btn btn-info browse" id="browse3" onclick="$('#pic3').click();">Browse</button></span>
+              <span class="input-group-btn"><button type="button" class="btn btn-cebroad" onclick="$('#pic3').click();">Browse</button></span>
             </div>
-            <label id="label3" class="events_label_color"></label>
-            <div class="pad_top">
+            <label id="label3" class="cebroad-pink"></label>
+            <div class="events-pad">
               <img src="" id="preview3" style="display:none; width: 300px;">
             </div>
         </div>
 
         <div class="form-group">
-            <input id=place_name type="hidden" name="place_name" value="">
-            <input id=lat type="hidden" name="lat" value="">
-            <input id=lng type="hidden" name="lng" value="">
+            <input id=place_name type="hidden" name="place_name" value="<?=h($place_name)?>">
+            <input id=lat type="hidden" name="lat" value="<?=h($lat)?>">
+            <input id=lng type="hidden" name="lng" value="<?=h($lng)?>">
         </div>
 
-        <div class="col-sm-8 col-md-8" class="pad_top";>
+        <?php foreach($errors as $error): ?>
+          <p class="cebroad-pink"><?=$error?></p>
+        <?php endforeach; ?>
+
+        <div class="col-sm-8 col-md-8" class="events-pad";>
             <div class="form-group">
-                <input type="submit" id="confirm" class="btn btn-primary" disabled="disabled" value="confirm">
+                <input type="submit" id="confirm" class="btn btn-cebroad" disabled="disabled" value="confirm">
+                <img src="/cebroad/webroot/assets/events/gif/loading.gif" id="loading" style="display: none;">
             </div>
         </div>
 
       </div>
     </form>
 </div>
-<script>
-
-function initialize() {
-    var input = document.getElementById('searchTextField');
-
-    var options = {
-        componentRestrictions: {country: 'ph'}
-    };
-    var autocomplete = new google.maps.places.Autocomplete(input, options);
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        var place = autocomplete.getPlace();
-        $('#place_name').attr('value', place.name);
-        $('#lat').attr('value', place.geometry.location.lat());
-        $('#lng').attr('value', place.geometry.location.lng());
-        $('#confirm').attr('disabled', false);
-        });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-//Placeに何か変更があった場合confirmを無効にする
-$('#searchTextField').keydown(function() {
-    $('#confirm').attr('disabled', true);
-});
-
-//3つある画像のプレビューを共通の関数で処理するためにそれぞれのidを取得する関数
-$('.pic').change(function(){
-    console.log('preview振り分け');
-    var picId = $(this).attr('id');
-    preview(picId);
-});
-//実際に画像のプレビューを行う関数
-function preview(id) {
-    console.log('preview');
-    var file = $('#' + id).prop('files')[0];
-    var num = id.slice(-1);
-    var previewId = 'preview' + num;
-    var labelId = 'label' + num;
-    var photoCoverId = 'photoCover' + num;
-    //png, jpg, jpegのどれにも一致しない場合注意文を表示
-    if ( file.type == 'image/png' || file.type == 'image/jpg' || file.type == 'image/jpeg') {
-        //空のimgタグにプレビューを挿入
-        var fr = new FileReader();
-        fr.onload = function() {
-            $('#' + previewId).attr('src', fr.result ).css('display','inline');
-            $('#' + labelId).text('');
-        }
-        fr.readAsDataURL(file);
-      } else {
-        $('#' + previewId).attr('src', '').css('display','none');
-        $('#' + labelId).text('You can choose only jpg or png file');
-      }
-      //display: none;で隠したinputタグのfile情報をphotoCoverに渡す
-        $('#' + photoCoverId).val($('#' + id).val().replace("C:\\fakepath\\", ""));
-    }
-    
-</script>
+<script src="/cebroad/webroot/assets/events/js/add.js"></script>
--- events/index.php
+++ events/index.php
@@ -8,17 +8,6 @@
 	 }
  ?>
 
-
-
-   <link rel="stylesheet" href="../webroot/assets/css/bootstrap.min.css">
-   <link rel="stylesheet" href="../webroot/assets/events/css/events.css">
-   <link rel="stylesheet" href="../webroot/assets/font-awesome/css/font-awesome.min.css">
-   <link href="../webroot/assets/events/css/events/styles.css" rel="stylesheet">
-    <style type="text/css">
-            @import url("http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,400italic");
-    </style>
-
-
     <div class="container">
 		<div class="row">
 		    <div class="testvar hidden-xs col-sm-2 col-md-2">
@@ -28,7 +17,7 @@
                     <div class="panel panel-default">
                     	<div class="panel-thumbnail"><img class="img-responsive" src="https://okinawa-labo.com/wp-content/uploads/2014/07/seaofoka.jpg" /></div>
                             <div class="panel-body">
-                                <p class="lead"><?php echo $event['event_name']; ?></p>
+                                <p class="lead"><?=h($event['title'])?></p>
                                	<p class="event_people"><i class="fa fa-users fa-lg"></i>:100</p>
 								<p class="event_like"><i class="fa fa-thumbs-o-up fa-lg"></i>:52</p>
                             </div>
