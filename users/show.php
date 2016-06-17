0e1a1c82ae0ad850760d1da7c467079b876702e2
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-06-17 11:18:18 +0800

    プロフィール画像部分修正

--- users/show.php
+++ users/show.php
@@ -1,21 +1,21 @@
 <?php
-//データベース接続
-  require('dbconnect.php');
-//関数
-  require('functions.php'); 
+// //データベース接続
+//   require('dbconnect.php');
+// //関数
+//   require('functions.php'); 
 
 
 // //セッションを使うページに必ず入れる
 //   session_start();
 
-//ログイン判定
-   //セッションにidが存在し、かつオンのtimeと3600秒足した値が現在時刻より小さい時に
-   //現在時刻より小さい時にログインしていると判定する
-   if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
-    //$_SESSIONに保存している時間更新
-    //これがないとログインから１時間たったら再度ログインしないとindex.phpに入れなくなる。
-    $_SESSION['time'] = time();
-	}
+// //ログイン判定
+//    //セッションにidが存在し、かつオンのtimeと3600秒足した値が現在時刻より小さい時に
+//    //現在時刻より小さい時にログインしていると判定する
+//    if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
+//     //$_SESSIONに保存している時間更新
+//     //これがないとログインから１時間たったら再度ログインしないとindex.phpに入れなくなる。
+//     $_SESSION['time'] = time();
+// 	}
 
 //ユーザー情報取得
   	$sql = sprintf('SELECT * FROM `users` WHERE `id`=%d', mysqli_real_escape_string($db, $_SESSION['id'])
@@ -39,7 +39,11 @@
     $record = mysqli_query($db, $sql) or die (mysqli_error($db));
     $school = mysqli_fetch_assoc($record); 
     
-    // var_dump($school);
+// var_dump($school);
+function h($value){
+      return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+
 
 ?>
 
@@ -62,7 +66,7 @@
             	</div>
 	            <div class="panel-body">
 	              	<div class="row">
-	                	<div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="profile_picture/<?php echo h($user['profile_picture_path']); ?>" class="img-circle img-responsive"> </div>
+	                	<div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="profile_pictures/<?php echo h($user['profile_picture_path']); ?>" class="img-responsive"> </div>
 		                <div class=" col-md-9 col-lg-9 "> 
 		                  	<table class="table table-user-information">
 			                    <tbody>
