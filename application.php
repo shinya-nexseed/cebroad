0bf5d61def0d582d1939cf2177c0181c4004ba2c
松澤 隼人 <hayatomatsuzawa@gmail.com>
2016-06-21 09:58:18 +0800

    cssファイル変更中

--- application.php
+++ application.php
@@ -11,6 +11,14 @@
 <head>
   <meta charset="UTF-8">
   <title>Cebroad</title>
+    <link href="/cebroad/webroot/assets/css/styles.css" rel="stylesheet">
+    <link href="../webroot/assets/css/users_show.css" rel="stylesheet">
+    <link href="../webroot/assets/css/bootstrap.css" rel="stylesheet">
+  <?php
+    function h($value){
+      return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+  ?> 
 </head>
 <body>
   <?php
