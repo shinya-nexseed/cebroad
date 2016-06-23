50d15c06fb426c389879d7e78ce9c99fb8513812
shinya-nexseed <shirai@nexseed.net>
2016-06-23 10:05:39 +0800

    routesファイル修正

--- routes.php
+++ routes.php
@@ -1,31 +1,32 @@
 <?php
-	session_start();
-	define('DEBUG', FALSE); //環境切り替え用 TRUEで開発環境モード
+  session_start();
+  define('DEBUG', FALSE); //環境切り替え用 TRUEで開発環境モード
 
-	$params = explode('/', $_GET['url']);
-	$resource = $params[0];
-	$action = "";
-	$id = 0;
-	$post = array();
-	if (isset($params[1])) {
-		$action = $params[1];
-	}
-	if (isset($params[2])) {
-		$id = $params[2];
-	}
-	if (isset($_POST) && !empty($_POST)) {
-		$post = $_POST;
-	}
+  $params = explode('/', $_GET['url']);
+  $resource = $params[0];
+  $action = "";
+  $id = 0;
+  $post = array();
+  if (isset($params[1])) {
+    $action = $params[1];
+  }
+  if (isset($params[2])) {
+    $id = $params[2];
+  }
+  if (isset($_POST) && !empty($_POST)) {
+    $post = $_POST;
+  }
 
   if ($resource === 'index' || $resource === '') {
     require('index.php');
   } else {
-    if($resource === 'login'){
-      require('login.php');
-    }
-    else if($resource === 'logout'){
-      require('logout.php');
-    } else{
+    if ($resource === 'signin') {
+      require('signin.php');
+    } else if ($resource === 'signout') {
+      require('signout.php');
+    } else if ($resource === 'join') {
+      require('views/join/join_layout.php'); 
+    } else {
       require('application.php');
     }
   }
