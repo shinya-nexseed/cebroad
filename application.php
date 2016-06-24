7654881d296e8981d10624e54585a150890fb757
shinya-nexseed <shirai@nexseed.net>
2016-06-24 11:40:28 +0800

    applicationファイルの修正 (chat未対応)

--- application.php
+++ application.php
@@ -1,4 +1,5 @@
 <?php
+
   if ( !function_exists('mime_content_type') ) {
       function mime_content_type($filename) {
           $mime_type = exec('file -Ib '.$filename);
@@ -7,13 +8,14 @@
   }
 
   if(isset($_SESSION['id'])) {
+
     //ログインしているユーザーのデータをDBから取得
     $sql = sprintf('SELECT *, schools.name AS school_name FROM `users` JOIN `schools` ON users.school_id=schools.id WHERE users.id=%d',
       $_SESSION['id']
       );
-
     $record = mysqli_query($db, $sql) or die('');
     $member = mysqli_fetch_assoc($record);
+
 
     //イベントカテゴリ呼び出し
     $sql=sprintf('SELECT * FROM `event_categories` WHERE 1');
@@ -38,6 +40,7 @@
       $event_users_makes[]=$result;
     }
 
+
     //ログインしているユーザーが参加するイベントの表示用にデータを取得
     $sql=sprintf('SELECT *FROM `events` 
       INNER JOIN `participants` ON events.id=participants.event_id
@@ -46,6 +49,7 @@
       );
 
     $record=mysqli_query($db, $sql)or die(mysqli_error($db));
+
     $event_users_parts=array();
 
     while($result=mysqli_fetch_assoc($record)){
@@ -117,305 +121,6 @@
       display: block;
     }
   </style>
-
-  <!-- <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet"> -->
-      <style type="text/css">
-          @import url(https://fonts.googleapis.com/css?family=Oswald:400,300);
-  @import url(https://fonts.googleapis.com/css?family=Open+Sans);
-  body
-  {
-  font-family: 'Open Sans', sans-serif;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message > li + li {
-      margin-top: 4px;
-  }
-  .popup-box-on {
-      display: block !important;
-  }
-  a:focus {
-      outline: none;
-      outline-offset: 0px;
-  }
-  .popup-head-left.pull-left h1 {
-      color: #fff;
-      float: left;
-      font-family: oswald;
-      font-size: 18px;
-      margin: 2px 0 0 5px;
-     
-  }
-  .popup-head-left a small {
-      display: table;
-      font-size: 11px;
-      color: #fff;
-      line-height: 4px;
-      opacity: 0.5;
-      padding: 0 0 0 7px;
-  }
-  .chat-header-button {
-      background: transparent none repeat scroll 0 0;
-      border: 1px solid #fff;
-      border-radius: 7px;
-      font-size: 15px;
-      height: 26px;
-      opacity: 0.9;
-      padding: 0;
-      text-align: center;
-      width: 26px;
-  }
-  .popup-head-right {
-      margin: 9px 0 0;
-  }
-  .popup-head .btn-group {
-      margin: -5px 3px 0 -1px;
-  }
-  .gurdeepoushan .dropdown-menu {
-      padding: 6px;
-  }
-  .gurdeepoushan .dropdown-menu li a span {
-      border: 1px solid;
-      border-radius: 50px;
-      display: list-item;
-      font-size: 19px;
-      height: 40px;
-      line-height: 36px;
-      margin: auto;
-      text-align: center;
-      width: 40px;
-  }
-  .gurdeepoushan .dropdown-menu li {
-      float: left;
-      text-align: center;
-      width: 33%;
-  }
-  .gurdeepoushan .dropdown-menu li a {
-      border-radius: 7px;
-      font-family: oswald;
-      padding: 3px;
-     transition: all 0.3s ease-in-out 0s;
-  }
-  .gurdeepoushan .dropdown-menu li a:hover {
-      background:#ff2a50 none repeat scroll 0 0 !important;
-      color: #fff;
-  }
-  .popup-head {
-      background: #ff2a50 none repeat scroll 0 0 !important;
-      border-bottom: 3px solid #ccc;
-      color: #fff;
-      display: table;
-      width: 100%;
-      padding: 8px;
-  }
-  .popup-head .md-user-image {
-      border: 2px solid #5a7172;
-      border-radius: 12px;
-      float: left;
-      width: 44px;
-  }
-  .uk-input-group-addon .glyphicon.glyphicon-send {
-      color: #ffffff;
-      font-size: 21px;
-      line-height: 36px;
-      padding: 0 6px;
-  }
-  .chat_box_wrapper.chat_box_small.chat_box_active {
-      
-      height: 342px;
-      overflow-y: scroll;
-      width: 316px;
-  }
-  aside {
-       background-attachment: fixed;
-      background-clip: border-box;
-      background-color: rgba(0, 0, 0, 0);
-      background-color:lightgrey ;
-      background-origin: padding-box;
-      background-position: center top;
-      background-repeat: repeat;
-      border: 1px solid #ff2a50;
-      bottom: 0;
-      display: none;
-      height: 466px;
-      position: fixed;
-      right: 70px;
-      width: 300px;
-      font-family: 'Open Sans', sans-serif;
-  }
-  .chat_box {
-      padding: 16px;
-  }
-  .chat_box .chat_message_wrapper::after {
-      clear: both;
-  }
-  .chat_box .chat_message_wrapper::after, .chat_box .chat_message_wrapper::before {
-      content: " ";
-      display: table;
-  }
-  .chat_box .chat_message_wrapper .chat_user_avatar {
-      float: left;
-  }
-  .chat_box .chat_message_wrapper {
-      margin-bottom: 32px;
-  }
-  .md-user-image {
-      border-radius: 50%;
-      width: 34px;
-  }
-  img {
-      border: 0 none;
-      box-sizing: border-box;
-      height: auto;
-      max-width: 100%;
-      vertical-align: middle;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message, .chat_box .chat_message_wrapper ul.chat_message > li {
-      list-style: outside none none;
-      padding: 0;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message {
-      float: left;
-      margin: 0 0 0 20px;
-      max-width: 77%;
-  }
-  .chat_box.chat_box_colors_a .chat_message_wrapper ul.chat_message > li:first-child::before {
-      border-right-color: #616161;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message > li:first-child::before {
-      border-color: transparent #ededed transparent transparent;
-      border-style: solid;
-      border-width: 0 16px 16px 0;
-      content: "";
-      height: 0;
-      left: -14px;
-      position: absolute;
-      top: 0;
-      width: 0;
-  }
-  .chat_box.chat_box_colors_a .chat_message_wrapper ul.chat_message > li {
-      background: #FCFBF6 none repeat scroll 0 0;
-      color: #000000;
-  }
-  .open-btn {
-      border: 2px solid #ff2a50;
-      border-radius: 32px;
-      color: #ff2a50 !important;
-      display: inline-block;
-      margin: 10px 0 0;
-      padding: 9px 16px;
-      text-decoration: none !important;
-      text-transform: uppercase;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message > li {
-      background: #ededed none repeat scroll 0 0;
-      border-radius: 4px;
-      clear: both;
-      color: #212121;
-      display: block;
-      float: left;
-      font-size: 13px;
-      padding: 8px 16px;
-      position: relative;
-      word-break: break-all;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message, .chat_box .chat_message_wrapper ul.chat_message > li {
-      list-style: outside none none;
-      padding: 0;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message > li {
-      margin: 0;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message > li p {
-      margin: 0;
-  }
-  .chat_box.chat_box_colors_a .chat_message_wrapper ul.chat_message > li .chat_message_time {
-      color: rgba(185, 186, 180, 0.9);
-  }
-  .chat_box .chat_message_wrapper ul.chat_message > li .chat_message_time {
-      color: #727272;
-      display: block;
-      font-size: 11px;
-      padding-top: 2px;
-      text-transform: uppercase;
-  }
-  .chat_box .chat_message_wrapper.chat_message_right .chat_user_avatar {
-      float: right;
-  }
-  .chat_box .chat_message_wrapper.chat_message_right ul.chat_message {
-      float: right;
-      margin-left: 0 !important;
-      margin-right: 24px !important;
-  }
-  .chat_box.chat_box_colors_a .chat_message_wrapper.chat_message_right ul.chat_message > li:first-child::before {
-      border-left-color: #ffa5b5;
-  }
-  .chat_box.chat_box_colors_a .chat_message_wrapper ul.chat_message > li:first-child::before {
-      border-right-color: #FCFBF6;
-  }
-  .chat_box .chat_message_wrapper.chat_message_right ul.chat_message > li:first-child::before {
-      border-color: transparent transparent transparent #ededed;
-      border-width: 0 0 29px 29px;
-      left: auto;
-      right: -14px;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message > li:first-child::before {
-      border-color: transparent #ededed transparent transparent;
-      border-style: solid;
-      border-width: 0 29px 29px 0;
-      content: "";
-      height: 0;
-      left: -14px;
-      position: absolute;
-      top: 0;
-      width: 0;
-  }
-  .chat_box.chat_box_colors_a .chat_message_wrapper.chat_message_right ul.chat_message > li {
-      background: #ffa5b5 none repeat scroll 0 0;
-  }
-  .chat_box .chat_message_wrapper ul.chat_message > li {
-      background: #ededed none repeat scroll 0 0;
-      border-radius: 12px;
-      clear: both;
-      color: #212121;
-      display: block;
-      float: left;
-      font-size: 13px;
-      padding: 8px 16px;
-      position: relative;
-  }
-  .gurdeep-chat-box {
-      background: #fff none repeat scroll 0 0;
-      border-radius: 5px;
-      float: left;
-      padding: 3px;
-      width: 243px;
-  }
-  #submit_message {
-      background: transparent none repeat scroll 0 0;
-      border: medium none;
-      padding: 4px;
-  }
-  .gurdeep-chat-box i {
-      color: #333;
-      font-size: 21px;
-      line-height: 1px;
-  }
-  .chat_submit_box {
-      bottom: 0;
-      box-sizing: border-box;
-      left: 0;
-      overflow: hidden;
-      padding: 10px;
-      position: absolute;
-      width: 100%;
-  }
-  .uk-input-group {
-      border-collapse: separate;
-      display: table;
-      position: relative;
-  }
-      </style>
-      <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 </head>
 <body>
   <div class="wrapper">
@@ -526,13 +231,13 @@
                           </ul>
                         </li>
 
+
                         </ul>
 
                         <div class="visible-xs">
                           <!-- <li style="display:inline-block;"> -->
                             <ul class="nav navbar-nav navbar-form navbar-right">
                               <li class="dropdown" style="display:inline-block;">
-
                                   <a href="" class="dropdown-toggle" data-toggle="dropdown"  class="navbar-right">
                                     <img src="/portfolio/cebroad/views/users/profile_pictures/<?php echo $member['profile_picture_path']; ?>" class="img-responsive" style="height:100%; width:30px;" alt="">
                                   </a>
@@ -556,7 +261,6 @@
                                                       <p class="text-center">
                                                          <form method="get">
                                                             <div class="input-group input-group-sm" style="max-width:200px;">
-
                                                               <select class="form-control" name="srch-term-categories" class="form-control">
                                                               <option value="0" selected>Select Category</option>
                                                               <?php
@@ -586,7 +290,6 @@
                                               <div class="row">
                                                   <div class="col-lg-12">
                                                       <p>
-
                                                            <a href="/portfolio/cebroad/users/edit"><button type="button" class="btn btn-success btn-sm">User Edit</button></a><br>
                                                           <a href="/portfolio/cebroad/events/add"><button type="button" class="btn btn-primary btn-sm">Make Event</button></a>
                                                       </p>
@@ -595,7 +298,6 @@
                                           </div>
                                       </li>
                                       <li>
-
                                         <a href="/portfolio/cebroad/logout"><span class="badge">SignOut</span></a>
                                       </li>
                                   </ul>
@@ -618,16 +320,13 @@
                           <div class="profile-sidebar">
                             <!-- SIDEBAR USERPIC -->
                             <div class="profile-userpic">
-
                               <img src="/portfolio/cebroad/views/users/profile_pictures/<?php echo $member['profile_picture_path']; ?>" class="img-responsive" alt=""><br>
                             </div>
                             <!-- END SIDEBAR USERPIC -->
 
                             <!-- SIDEBAR BUTTONS -->
-
                               <a href="/portfolio/cebroad/users/edit"><button type="button" class="btn btn-success btn-sm">User Edit</button></a>
                               <a href="/portfolio/cebroad/events/add"><button type="button" class="btn btn-primary btn-sm">Make Event</button></a>
-
                               <br>
                               <br>
                             
@@ -644,7 +343,6 @@
                                       echo $date;
                                       echo '：';
                                       ?>
-
                                       <a href="/portfolio/cebroad/events/show/<?php echo $event_users_make['id']?>"><?php  echo $event_users_make['title'];?></a></p>
                                     <?php } ?>
                                   </div>
@@ -663,7 +361,6 @@
                                       echo $date;
                                       echo '：';
                                       ?>
-
                                       <a href="/portfolio/cebroad/events/show/<?php echo $event_users_part['id']?>">
                                       <?php echo $event_users_part['title'];?></a></p>
                                     <?php } ?>
@@ -679,6 +376,7 @@
                       <!-- content -->                      
                       <div class="column col-sm-10">
                         <div class="row">
+                          
                           <?php
                               $url = dirname(__FILE__).'/views/'.$resource.'/'.$action.'.php';
                               // echo $url;
@@ -702,14 +400,11 @@
       </div>
   </div>
 
-  <?php 
-    if ($resource === 'events' && $action === 'show') {
-      require(dirname(__FILE__).'/views/events/chat.php');
-    }
-   ?>
-
-
   <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
   <script type="text/javascript" src="/portfolio/cebroad/webroot/assets/js/bootstrap.js"></script>
 </body>
 </html>
+<script src="/cebroad/webroot/assets/js/jquery.min.js"></script>
+<script src="/cebroad/webroot/assets/js/jquery.Jcrop.js"></script>
+<script src="/cebroad/webroot/assets/js/inputfile.js"></script>
+<script src="/cebroad/webroot/assets/js/crop.js"></script>
